9f0a5c489ca15ee9dac1de7421ea93ab9dffc77c
= <=>
2025-09-17 10:29:50 +0200

    PyCharm update

---
+++ zad2/zad1.py
@@ -0,0 +1,4 @@
+sum = 0.0
+for i in range(1, 11):
+    sum += float(input("Podaj liczbę do zsumowania: "))
+    print(f"Bierząca suma po {i} iteracjach liczb podanych przez ciebie wynosi: {sum:.2f}")
---
+++ zad2/zad2.py
@@ -0,0 +1,4 @@
+sum = 0
+for i in range(30):
+    sum = 0
+print(f"Suma równania wynosi: {sum}")
