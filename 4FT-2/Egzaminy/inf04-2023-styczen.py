47293c26e95d20a2678c2d72b5eafa7aa3381cbd
Saybusch <insglitch@gmail.com>
2026-02-11 07:55:37 +0100

    2026-02-11

--- 4FT-2/Egzaminy/inf04-2023-styczen.py
+++ 4FT-2/Egzaminy/inf04-2023-styczen.py
@@ -22,14 +22,27 @@ class Notatka:
         print(f"Treść: {self._tresc}")
     def diagnostyka(self):
         print(f"{self.__id};{self._tytul};{self._tresc}")
+class NotatkaZObrazem(Notatka):
+    def __init__(self, tytul, tresc, sciezka_obrazka):
+        super().__init__(tytul, tresc)
+        self._sciezka_obrazka = sciezka_obrazka
+    def wyswietl(self):
+        super().wyswietl()
+        print(f"Obrazek: {self._sciezka_obrazka}")
+    def diagnostyka(self):
+        super().diagnostyka()
+        print(f"Scieżka obrazka: {self._sciezka_obrazka}")
+
 if __name__ == "__main__":
     print("Program testujący działanie klasy Notatka")
     not1 = Notatka("Zakupy", "Kupić mleko, chleb i owoce.")
     not2 = Notatka("Plan dnia", "Nauka Pythona, spacer, film wieczorem.")
+    not3 = NotatkaZObrazem("Pomysł", "Stworzyć aplikacje", "images/img1")
     print("\nWyświetlanie notatek:")
     not1.wyswietl()
     not2.wyswietl()
+    not3.wyswietl()
     print("\nDiagnostyka notatek:")
     not1.diagnostyka()
     not2.diagnostyka()
-
+    not3.diagnostyka()
---
+++ 4FT-2/Egzaminy/inf04-2025-styczen.py
@@ -0,0 +1,39 @@
+class Urzadzenia:
+    def komunikat(self, message):
+        print(message)
+class Pralka(Urzadzenia):
+    def __init__(self):
+        self.__numerProgramuPrania = 0
+    def ustawProgram(self, number):
+        if 1 <= number <= 12:
+            self.__numerProgramuPrania = number
+            self.komunikat("Program został ustawiony")
+        else:
+            self.__numerProgramuPrania = 0
+            self.komunikat("Podano niepoprawny numer programu")
+
+class Odkurzacz(Urzadzenia):
+    def __init__(self):
+        self.__state = False
+    def on(self):
+        if not self.__state:
+            self.__state = True
+            self.komunikat("Odkurzacz włączono")
+    def off(self):
+        if self.__state:
+            self.__state = False
+            self.komunikat("Odkurzacz wyłączono")
+if __name__ == "__main__":
+    pralka = Pralka()
+    odkurzacz = Odkurzacz()
+
+    value = int(input("Podaj numer prania 1..12\n"))
+    pralka.ustawProgram(value)
+
+    odkurzacz.on()
+    odkurzacz.on()
+    odkurzacz.on()
+
+    odkurzacz.komunikat("Odkurzacz wyładował się")
+
+    odkurzacz.off()
