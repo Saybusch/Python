ddfd7d7fe5fe7e004eaa21085c33e264e135498a
Saybusch <insglitch@gmail.com>
2026-03-25 07:56:30 +0100

    2026-03-25

--- 4FT-2/Egzaminy/inf04-2024-styczen.py
+++ 4FT-2/Egzaminy/inf04-2024-styczen.py
@@ -1,15 +1,39 @@
+'''
+************************************************
+ nazwa: <tu wstaw nazwę funkcji / metody>
+ opis: <co wykonuje funkcja / metoda?>
+ parametry: <opis parametru1, lub „brak”>
+<opis parametru2>
+...
+ zwracany typ i opis:<nazwa typu i opis co jest zwracane lub „brak”>
+ autor: <numer zdającego>
+************************************************
+'''
+
 import random
-if __name__ == "__main__":
-    array = []
-    dice = 0
+def losuj(amount):
+    tablica = []
+    for i in range(amount):
+        tablica.append(random.randint(1, 6))
+        print(f"Kostka {i + 1}: {tablica[i]}")
+    return tablica
+def licz(tablica):
     suma = 0
-    while 3 < dice > 6:
-        dice = int(input("Ile kostek chcesz rzucić?(3 - 10)"))
-        for i in range(dice):
-            array.append(random.randint(1, 6))
-            print(f"Kostak {i}: {array[i]}")
-        for i in range(1, 6):
-            ilosc = array.count(i)
-            if ilosc > 1:
-                suma += i * ilosc
-    print(suma)
+    for i in set(tablica):
+        ilosc = tablica.count(i)
+        if ilosc > 1:
+            suma += i * ilosc
+    return suma
+if __name__ == "__main__":
+    loop = True
+    while loop:
+        dice = 0
+        while dice < 3 or dice > 10:
+            dice = int(input("Ile kostek chcesz rzucić?(3 - 10)\n"))
+        array = losuj(dice)
+        print(f"Liczba uzyskanych punktów: {licz(array)}")
+        response = input("Jeszcze raz? (t/n)\n")
+        if response == "t":
+            loop = True
+        elif response == "n":
+            loop = False
