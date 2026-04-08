6b8ef574fda4d4846a2b6650d3de06e5a8e6a33e
Saybusch <insglitch@gmail.com>
2026-04-08 07:57:00 +0200

    2026-04-08

---
+++ 4FT-2/SortingAlgorythms/BubbleSort.py
@@ -0,0 +1,16 @@
+import random
+class BubbleSort:
+    @staticmethod
+    def generuj_tablice(n):
+        return [random.randint(0, 1000) for x in range(n)]
+    @staticmethod
+    def sortuj(tablica):
+        length = len(tablica)
+        for i in range(length - 1):
+            for j in range(length - 1 - i):
+                if tablica[j] > tablica[j + 1]:
+                    tablica[j], tablica[j + 1] = tablica[j + 1], tablica[j]
+                    print("Zamiania: ", tablica[j], tablica[j + 1])
+        return tablica
+if __name__ == "__main__":
+    print(BubbleSort.sortuj([5, 1, 4, 8]))
