19d65e3a57e269c25bd30487746b8a2122a71e18
Saybusch <insglitch@gmail.com>
2026-04-21 12:22:35 +0200

    2026-04-21-2

--- 4FT-2/SearchingAlgorythms_zadanie/zad1.py
+++ 4FT-2/SearchingAlgorythms_zadanie/zad1.py
@@ -19,7 +19,18 @@ class WyszukiwanieNapisow:
     def znajdz_wszystkie(cls, find) -> list:
         return [x for x in range(len(cls.napisy)) if cls.napisy[x] == find]
     @classmethod
-    def znajdz_pierwszy_z_prefiksem(cls, prefix):
+    def znajdz_pierwszy_z_prefiksem1(cls, prefix):
+        for el in cls.napisy:
+            pasuje = len(prefix) < len(el)
+            for i in range(len(prefix)):
+                if el[i] != prefix[i]:
+                    pasuje = False
+                    break
+            if pasuje:
+                return el
+        return None
+    @classmethod
+    def znajdz_pierwszy_z_prefiksem2(cls, prefix):
         for el in cls.napisy:
             if el[:len(prefix)] == prefix:
                 return el
@@ -38,27 +49,7 @@ class WyszukiwanieNapisow:
                 maxim = item
                 maximKey = it
         return maximKey
-
 if __name__ == '__main__':
-    '''
-    amount = int(input("Ile napisów: "))
-    array = []
-    for i in range(1, amount+1):
-        array.append(input(f"Dana {i}: "))
-        if array[-1] == "" or len(array) != i:
-            array.pop()
-            i -= 1
-    istota = WyszukiwanieNapisow(array)
-    #t1 = input("Szukane słowo: ")
-    if istota.zawiera(t1):
-        print(f"Lista zawiera {t1}")
-    else:
-        print(f"Lista nie zawiera {t1}")
-    print(f"Pierwszy indeks elementu {t1}: {istota.indeks(t1)}")
-    print(f"Indeksy elementu {t1}: {istota.znajdz_wszystkie(t1)}")
-    t2 = input("Prefiks do słów: ")
-    print(f"Pierwszy napis zawierający prefiks {t2}: {istota.znajdz_pierwszy_z_prefiksem(t2)}")
-    '''
     array = ["ala", "kot", "ala", "pies", "kot"]
     istota = WyszukiwanieNapisow(array)
     print(istota.zawiera("ala"))
@@ -66,4 +57,5 @@ if __name__ == '__main__':
     print(istota.znajdz_wszystkie("ala"))
     print(istota.licz_wystapienia("kot"))
     print(istota.najczestszy())
-    print(istota.znajdz_pierwszy_z_prefiksem("pi"))
+    print(istota.znajdz_pierwszy_z_prefiksem1("pi"))
+    print(istota.znajdz_pierwszy_z_prefiksem2("pi"))
